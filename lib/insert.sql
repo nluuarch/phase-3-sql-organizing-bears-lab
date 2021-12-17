240b2ce2f640b3574d21fc1988ae0c80bd088ad8
nluuarch@gmail.com <nluuarch@gmail.com>
2021-12-16 17:24:12 -0800

    commit tree 1fda03dfe5d1406e9b471307ccfedd617d42dd64

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 40, 'M', "Brown", "Nice", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 25, 'M', "Black", "Chill", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 1, 'F', "Red", "Chill", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 20, 'M', "Brown", "Mean", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 1, 'F', "White", "Chill", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 10, 'M', "Black", "Mean", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 15, 'F', "Red", "Nice", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 2, 'M', "Brown", "Mean", False);
+
